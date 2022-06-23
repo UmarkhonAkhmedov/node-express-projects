b190c733257c3569b7a1c41dea5d5ed02a9564eb
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-06-23 19:12:13 +0500

    Changing the variable

--- task-manager/db/connect.js
+++ task-manager/db/connect.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose')
-const connectionString = 'mongodb+srv://Thomas:qwerty12345@nodeexpressprojects.ghw5idm.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority'
 
 
 const connectDB = (url) => {
   return (
     mongoose
-      .connect(connectionString, {
+      .connect(url, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
