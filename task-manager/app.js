89e452d000c8c4b1ab868853e1eb5bd38a9557f4
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-06-23 19:05:40 +0500

    Changing the way of connecting to the MongoDB

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
-require('./db/connect')
+const connectDB = require('./db/connect')
 
 
 // Middleware
@@ -23,8 +23,19 @@ app.use('/api/v1/tasks', tasks)
 // app.delete('/api/v1/tasks/:id')  - delete the task
 
 
-// Listing the port
 const port = 3000
-app.listen(port, console.log(`server is listening on port ${port}...`))
+
+const start = async () => {
+  try {
+    await connectDB()
+    app.listen(port, console.log(`server is listening on port ${port}...`))
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start()
+
+
 
 
--- task-manager/db/connect.js
+++ task-manager/db/connect.js
@@ -1,18 +1,23 @@
 const mongoose = require('mongoose')
-
-
 const connectionString = 'mongodb+srv://Thomas:qwerty12345@nodeexpressprojects.ghw5idm.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority'
 
 
-mongoose
-  .connect(connectionString, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('CONNECTED TO THE DB...'))
-  .catch((err) => console.log(err))
+const connectDB = (url) => {
+  return (
+    mongoose
+      .connect(connectionString, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      })
+  )
+}
+
+
+module.exports = connectDB
+
+
 
 
 
