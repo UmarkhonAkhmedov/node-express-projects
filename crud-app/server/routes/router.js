4ac7374c1f27559b6e3378ea11af546965c3c397
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-06-30 19:05:01 +0500

    Creating all controllers for CRUD operations for users

--- crud-app/server/routes/router.js
+++ crud-app/server/routes/router.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const route = express.Router()
 const services = require('../services/render')
+const controller = require('../controller/controller')
 
 
 // @description Root Route 
@@ -11,6 +12,12 @@ route.get("/add-user", services.add_user)
 
 // @description Update User
 route.get("/update-user", services.update_user)
+
+// API
+route.post('/api/users', controller.create)
+route.get('/api/users', controller.find)
+route.put('/api/users/:id', controller.update)
+route.delete('/api/users/:id', controller.delete)
 
 
 
