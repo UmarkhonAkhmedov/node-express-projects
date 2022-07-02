3c00098232d49bad75dd9a816f3e6b95101394df
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-07-02 06:53:50 +0500

    Creating JWT project, inital files

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -2,13 +2,12 @@ const Product = require('../models/product')
 
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'ab'
-  const products = await Product.find({name: {$regex:search, $options: 'i'}})
+  const products = await Product.find({price:{$gt:30}}).select('name price')
   res.status(200).json({ products, nbHits:products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query
+  const { featured, company, name, sort, fields, numericFilters } = req.query
   const queryObject = {}
   if(featured){
     queryObject.featured = featured === "true" ? true : false
@@ -19,8 +18,34 @@ const getAllProducts = async (req, res) => {
   if(name){
     queryObject.name = { $regex: name, $options: 'i' }
   }
-  console.log(queryObject);
-  const products = await Product.find(queryObject)
+  let result = Product.find(queryObject)
+  if(sort){
+    const sortList = sort.split(',').join('')
+    result = result.sort(sortList)
+  }else {
+    result = result.sort('createdAt')
+  }
+  if(fields){
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
+  }
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  result = result.limit(limit)
+
+  if(numericFilters){
+    const operatorMap = {
+      '>':'$gt',
+      '>=':'$gte',
+      '=':'$eq',
+      '<':'$lt', 
+      '<=':'$lte', 
+    }
+    const regEx = /\b(<|>|>=|<=|)\b/g
+    let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
+  }
+
+  const products = await result
   res.status(200).json({ products, nbHits:products.length })
 }
 
