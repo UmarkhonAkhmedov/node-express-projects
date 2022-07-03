561671eafc998b4b31160220daa669cd9caea33c
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-07-03 07:32:02 +0500

    Finishing Login and Regsiter functionalities

---
+++ Jobs API/controllers/jobs.js
@@ -0,0 +1,25 @@
+const getAllJobs = async (req, res) => {
+  res.send('get all jobs')
+}
+
+const getJob = async (req, res) => {
+  res.send('get all jobs')
+}
+
+const createJob = async (req, res) => {
+  res.json(req.user)
+}
+
+const updateJob = async (req, res) => {
+  res.send('get all jobs')
+}
+
+const deleteJob = async (req, res) => {
+  res.send('get all jobs')
+}
+
+
+module.exports = {
+  getAllJobs, getJob, createJob, updateJob, deleteJob
+}
+
